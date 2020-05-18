f286e1868b882056db76d59bc0e688b462661986
LiTrevize <universeofjimmy@gmail.com>
2020-05-18 21:48:24 +0800

    gexf py update

--- data/generate_gexf.py
+++ data/generate_gexf.py
@@ -32,37 +32,50 @@ def gen_edge_csv(out, dblp_only=True):
                     write_csv(g, r, idx)
 
 
-def gexf_from_csv(node_csv, edge_csv):
+def gexf_from_csv(node_csv, edge_csv, out):
     gexf = Gexf("Jingyu", "The DBLP Citation Network")
     graph = gexf.addGraph("directed", "static", "The DBLP Citation Network")
 
     # attr_dblp = graph.addNodeAttribute('isDBLP', 'false', type='boolean')
 
     print('adding nodes...')
-    tot = count_lines(node_csv)
-    with open(node_csv, 'r', encoding='utf8') as f:
-        for i, line in enumerate(f):
-            idx, title = line.strip().split('\t')
-            node = graph.addNode(str(idx), title)
-            # node.addAttribute(attr_dblp, 'true')
-            if i % 10000 == 0:
-                print('\r{:.2f}%'.format(i * 100 / tot), end='')
-        print()
+    if node_csv:
+        tot = count_lines(node_csv)
+        with open(node_csv, 'r', encoding='utf8') as f:
+            for i, line in enumerate(f):
+                idx, title = line.strip().split('\t')
+                node = graph.addNode(str(idx), title)
+                # node.addAttribute(attr_dblp, 'true')
+                if i % 10000 == 0:
+                    print('\r{:.2f}%'.format(i * 100 / tot), end='')
+            print()
+    else:
+        tot = count_lines(edge_csv)
+        nodes = set()
+        with open(edge_csv, 'r', encoding='utf8') as f:
+            for i, line in enumerate(f):
+                src, tar = line.strip().split('\t')
+                nodes.add(src)
+                nodes.add(tar)
+                if i % 10000 == 0:
+                    print('\r{:.2f}%'.format(i * 100 / tot), end='')
+            print()
+
+        for nid in nodes:
+            node = graph.addNode(nid, nid)
 
     print('adding edges...')
     tot = count_lines(edge_csv)
-    num_edge = 0
     with open(edge_csv, 'r', encoding='utf8') as f:
         for i, line in enumerate(f):
             src, tar = line.strip().split('\t')
-            graph.addEdge(str(num_edge), str(src), str(tar))
-            num_edge += 1
+            graph.addEdge(str(i), str(src), str(tar))
             if i % 10000 == 0:
                 print('\r{:.2f}%'.format(i * 100 / tot), end='')
         print()
 
     print('writing to file...')
-    with open('dblp_raw.gexf', 'wb') as g:
+    with open(out, 'wb') as g:
         gexf.write(g)
 
 
@@ -72,4 +85,6 @@ if __name__ == "__main__":
     # gen_node_csv('node_dblp_full.csv', False)
     # gen_edge_csv('edge_dblp_full.csv', False)
     # gexf_from_csv('node_dblp_only.csv','edge_dblp_only.csv')
+    # gexf_from_csv(None,'dblp/community/108979965.csv','test.gexf')
+    gexf_from_csv(None,'dblp/community/460513896.csv','test1.gexf')
     pass
